c6219d646fb0cd2de0c6306e06c699b0bd6b3ead
flubzie <0xflubzie@gmail.com>
2024-09-20 21:35:59 -0400

    new line

--- init.lua
+++ init.lua
@@ -971,15 +971,21 @@ local function telescope_vim_keybindings()
   local action_state = require 'telescope.actions.state'
 
   local mode_labels = {
-    n = 'Normal', i = 'Insert', v = 'Visual', x = 'Visual Block',
-    s = 'Select', o = 'Operator-pending', c = 'Command-line', t = 'Terminal',
+    n = 'Normal',
+    i = 'Insert',
+    v = 'Visual',
+    x = 'Visual Block',
+    s = 'Select',
+    o = 'Operator-pending',
+    c = 'Command-line',
+    t = 'Terminal',
   }
 
   local keybindings = {}
   local max_len_lhs = 0
   local max_len_mode = 0
 
-  local common_vim_keybindings = require('common_keybindings')
+  local common_vim_keybindings = require 'common_keybindings'
 
   for _, binding in ipairs(common_vim_keybindings) do
     table.insert(keybindings, binding)
@@ -1008,45 +1014,45 @@ local function telescope_vim_keybindings()
   end
 
   local function make_display(entry)
-    local mode = string.format("%-" .. max_len_mode .. "s", entry.mode)
-    local lhs = string.format("%-" .. (max_len_lhs + 15) .. "s", entry.lhs:gsub('%s', '<Space>'))
-    local desc = string.format("%-65s", entry.desc:sub(1, 65))
+    local mode = string.format('%-' .. max_len_mode .. 's', entry.mode)
+    local lhs = string.format('%-' .. (max_len_lhs + 15) .. 's', entry.lhs:gsub('%s', '<Space>'))
+    local desc = string.format('%-65s', entry.desc:sub(1, 65))
     local rhs = (entry.rhs or ''):gsub('\n', '\\n'):sub(1, 40)
-    return string.format("%s │ %s │ %s │ %s", mode, lhs, desc, rhs)
+    return string.format('%s │ %s │ %s │ %s', mode, lhs, desc, rhs)
   end
 
-  pickers.new({}, {
-    prompt_title = 'Vim Keybindings',
-    finder = finders.new_table {
-      results = keybindings,
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = make_display(entry),
-          ordinal = entry.mode .. ' ' .. entry.lhs .. ' ' .. entry.desc .. ' ' .. (entry.rhs or ''),
-        }
+  pickers
+    .new({}, {
+      prompt_title = 'Vim Keybindings',
+      finder = finders.new_table {
+        results = keybindings,
+        entry_maker = function(entry)
+          return {
+            value = entry,
+            display = make_display(entry),
+            ordinal = entry.mode .. ' ' .. entry.lhs .. ' ' .. entry.desc .. ' ' .. (entry.rhs or ''),
+          }
+        end,
+      },
+      sorter = conf.generic_sorter {},
+      attach_mappings = function(prompt_bufnr, map)
+        actions.select_default:replace(function()
+          actions.close(prompt_bufnr)
+          local selection = action_state.get_selected_entry()
+          vim.api.nvim_put({ make_display(selection.value) }, '', false, true)
+        end)
+        return true
       end,
-    },
-    sorter = conf.generic_sorter{},
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local selection = action_state.get_selected_entry()
-        vim.api.nvim_put({make_display(selection.value)}, '', false, true)
-      end)
-      return true
-    end,
-  }):find()
+    })
+    :find()
 end
 
-
-local wk = require("which-key")
-wk.add({
-  {"<leader>sK", telescope_vim_keybindings, desc = 'Search All Keybindings'}
-})
+local wk = require 'which-key'
+wk.add {
+  { '<leader>sK', telescope_vim_keybindings, desc = 'Search All Keybindings' },
+}
 
 vim.keymap.set('n', '<leader>sK', telescope_vim_keybindings, { desc = '[S]earch All [K]eybindings' })
-
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
